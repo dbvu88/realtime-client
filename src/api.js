0628ac743dbd6c03ba0b5b1d8e7db58b68a88817
Duc Vu <de.yd315@gmail.com>
2019-10-06 10:32:55 -0500

    hardcoded the url

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const url = process.env.REACT_APP_SOCKET;
 console.log(url);
-const socket = io(url);
+const socket = io("https://ducrealtime.herokuapp.com/");
 
 const subscribeToTimer = cb => {
   socket.on("timer", timestamp => cb(timestamp));
