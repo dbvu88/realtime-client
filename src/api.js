e6cc33b3b1a3df7fd0f025f4ea07de4cc546b6d1
Duc Vu <de.yd315@gmail.com>
2019-10-06 09:42:10 -0500

    changed socket url

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-import openSocket from "socket.io-client";
+import io from "socket.io-client";
 require("dotenv").config();
 
 const url = process.env.REACT_APP_SOCKET;
 console.log(url);
-const socket = openSocket("http://localhost:8000");
+const socket = io(url);
 
 const subscribeToTimer = cb => {
   socket.on("timer", timestamp => cb(timestamp));
