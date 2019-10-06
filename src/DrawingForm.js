3d338df6a413074f3d756ac8eab80bd596106a04
Duc Vu <de.yd315@gmail.com>
2019-10-06 16:13:16 -0500

    created DrawingForm component

---
+++ src/DrawingForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { createDrawing } from "./api";
+
+const DrawingFrom = props => {
+  const [drawingName, setDrawingName] = useState("");
+
+  return (
+    <div className="Form">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          createDrawing(drawingName);
+          setDrawingName("");
+        }}
+      >
+        <input
+          type="text"
+          value={drawingName}
+          onChange={e => setDrawingName(e.target.value)}
+          placeholder="Drawing name"
+          className="Form-drawingInput"
+          required
+        />
+        <button type="submit" className="Form-drawingInput">
+          Create
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default DrawingFrom;
